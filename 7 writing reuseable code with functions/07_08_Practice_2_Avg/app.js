3265125006075f60a149595124055b611d4df475
d1sc0 <9902327+d1sc0@users.noreply.github.com>
2020-04-05 19:28:47 +0100

    Add files via upload
    updated

--- 7 writing reuseable code with functions/07_08_Practice_2_Avg/app.js
+++ 7 writing reuseable code with functions/07_08_Practice_2_Avg/app.js
@@ -1,3 +1,28 @@
 // Write a function to find the average value in an array of numbers
 //avg([0,50]) //25
 //avg([75,76,80,95,100]) //85.2
+
+//loop over each num
+//add them up
+//divide by lenght of nums
+
+function avg(arr) {
+  let total = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+    total += num; //add num to a total
+  }
+
+  return total / arr.length;
+}
+
+function avg(arr) {
+  let total = 0;
+
+  for (let num of arr) {
+    total += num; //add num to a total
+  }
+
+  return total / arr.length;
+}
