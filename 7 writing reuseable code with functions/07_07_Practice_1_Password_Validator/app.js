f0852038effb3666b265abebcb904a2a89c35f3d
d1sc0 <9902327+d1sc0@users.noreply.github.com>
2020-04-05 19:28:07 +0100

    Add files via upload
    updated

--- 7 writing reuseable code with functions/07_07_Practice_1_Password_Validator/app.js
+++ 7 writing reuseable code with functions/07_07_Practice_1_Password_Validator/app.js
@@ -10,3 +10,49 @@
 // isValidPassword('89Fjj1nms', 'dogLuvr');  //true
 // isValidPassword('dogLuvr123!', 'dogLuvr') //false
 // isValidPassword('hello1', 'dogLuvr') //false
+function isValidPasswordOne(password, username) {
+  if (password.length < 8) {
+    return false;
+  }
+  if (password.indexOf(" ") !== -1) {
+    return false;
+  }
+  if (password.indexOf(username) !== -1) {
+    return false;
+  }
+  return true;
+}
+
+function isValidPasswordTwo(password, username) {
+  if (
+    password.length < 8 ||
+    password.indexOf(" ") !== -1 ||
+    password.indexOf(username) !== -1
+  ) {
+    return false;
+  }
+  return true;
+}
+
+function isValidPasswordThree(password, username) {
+  const tooShort = password.length < 8;
+  const hasSpace = password.indexOf(" ") !== -1;
+  const contsUsername = password.indexOf(username) !== -1;
+  if (tooShort || hasSpace || contsUsername) return false;
+  return true;
+}
+
+function isValidPasswordFour(password, username) {
+  const tooShort = password.length < 8;
+  const hasSpace = password.indexOf(" ") !== -1;
+  const contsUsername = password.indexOf(username) !== -1;
+  if (!tooShort && !hasSpace && !contsUsername) return true;
+  return false;
+}
+
+function isValidPasswordFive(password, username) {
+  const tooShort = password.length < 8;
+  const hasSpace = password.indexOf(" ") !== -1;
+  const contsUsername = password.indexOf(username) !== -1;
+  return !tooShort && !hasSpace && !contsUsername;
+}
