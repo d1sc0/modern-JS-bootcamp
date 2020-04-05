72fa6aacf65e8c01884b907953ce01965bc5154d
d1sc0 <9902327+d1sc0@users.noreply.github.com>
2020-04-05 19:29:17 +0100

    Add files via upload
    updated

--- 7 writing reuseable code with functions/07_09_Practice_3_getCard/app.js
+++ 7 writing reuseable code with functions/07_09_Practice_3_getCard/app.js
@@ -4,7 +4,39 @@
 // 			suit: 'clubs'
 // 		}
 //Pick a random value from:
-//----1,2,3,4,5,6,7,8,9,10,J,Q,K,A
+//----2,3,4,5,6,7,8,9,10,J,Q,K,A
 //Pick a random suit from:
 //----clubs,spades, hearts, diamonds
 //Return both in an object
+
+function getCard() {
+  const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  const suits = ["clubs", "spades", "hearts", "diamonds"];
+
+  let valueIndex = Math.floor(Math.random() * values.length);
+  let suitIndex = Math.floor(Math.random() * suits.length);
+
+  let card = {
+    value: values[valueIndex],
+    suit: suits[suitIndex]
+  };
+
+  return card;
+}
+
+//-------------------------------------
+
+function getCardTwo() {
+  const values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  const suits = ["clubs", "spades", "hearts", "diamonds"];
+
+  return {
+    value: pickRandom(values),
+    suit: pickRandom(suits)
+  };
+}
+
+function pickRandom(arr) {
+  const idx = Math.floor(Math.random() * arr.length);
+  return arr[idx];
+}
